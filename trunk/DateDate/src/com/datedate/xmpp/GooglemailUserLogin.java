35f885233137de02206a18866839b32a467b62b0
realyoungdj@gmail.com <realyoungdj@gmail.com@b563fa21-1a75-1f20-c6b7-2c6086b1a6cc>
2011-09-13 15:38:09 +0000

    add Class UserLogin and subclass Gmail, Googlemail, Jabber UserLogin.

---
+++ trunk/DateDate/src/com/datedate/xmpp/GooglemailUserLogin.java
@@ -0,0 +1,37 @@
+package com.datedate.xmpp;
+
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Presence;
+
+import android.util.Log;
+
+public class GooglemailUserLogin extends UserLogin{
+	private XMPPConnection conn = null;
+	private static String logtag = "GooglemailLogin";
+	
+	public GooglemailUserLogin(String uid, String pwd) {
+		super(uid, pwd);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public XMPPConnection connectServer() {
+		ConnectionConfiguration cfg = new ConnectionConfiguration("talk.google.com", 5222, "googlemail.com");
+		cfg.setSASLAuthenticationEnabled(false);
+		conn = new XMPPConnection(cfg);
+    	try {    		
+			conn.connect();
+			conn.login(super.user, super.password);
+			Presence presence = new Presence(Presence.Type.available);
+			presence.setStatus("UsingDateDate");
+			conn.sendPacket(presence);			
+		} catch (XMPPException e) {
+			Log.v(logtag, "Error when login " + conn.getHost());
+			e.printStackTrace();
+			conn = null;
+		}    	
+		return conn;		
+	}
+
+}
--- trunk/DateDate/src/com/datedate/xmpp/UserLogin.java
+++ trunk/DateDate/src/com/datedate/xmpp/UserLogin.java
@@ -1,5 +1,26 @@
 package com.datedate.xmpp;
 
-public interface UserLogin {
+import org.jivesoftware.smack.XMPPConnection;
 
+
+public class UserLogin {
+	public String user;
+	public String password;
+	public String service;
+	public String userid;
+	
+	public UserLogin(String uid, String pwd) {
+		userid = uid;
+		int idx = uid.indexOf("@");
+		user = uid.substring(0, idx);
+		service = uid.substring(idx + 1, uid.length());
+		password = pwd;
+	}
+	
+	public XMPPConnection connectServer() {
+		//
+		return null;
+	}
+	
+	
 }
